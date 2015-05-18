1b1b9cd1e820b9509188466ed618e09316304b9e
Corey Leveen <mntj11@gmail.com>
2015-05-17 23:46:32 -0400

    Configure Rack with test.html

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'rack'
 
 app = Proc.new do |env|
-  ['200', {'Content-Type' => 'text/html'}, ['Barebones']]
+  [
+    '200',
+    {'Content-Type' => 'text/html'},
+    File.open("test.html", File::RDONLY)
+  ]
 end
 
 Rack::Handler::WEBrick.run app
