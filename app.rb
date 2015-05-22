a63adea3f363210d80c7c7f1d0f6ace448f5e81c
Corey Leveen <mntj11@gmail.com>
2015-05-21 23:49:06 -0400

    File uploads working

--- app.rb
+++ app.rb
@@ -1,12 +1,17 @@
 require 'sinatra'
+
 set :views, settings.root
 
 enable :sessions
+
 
 get '/' do
   haml :index
 end
 
 post '/upload' do
-  redirect to '/'
+  File.open('uploads/' << params['cameraInput'][:filename], "w") do |f|
+    f.write(params['cameraInput'][:tempfile].read)
+  end
+  "Uploaded #{params['cameraInput'][:filename]}!"
 end
