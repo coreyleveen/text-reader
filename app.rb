016bb6810b046bd734e1ca3007b59e867c2e2d6a
Corey Leveen <mntj11@gmail.com>
2015-06-09 01:44:57 -0400

    Add httmultiparty implementation

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'httmultiparty'
 
 enable :sessions
 
@@ -6,5 +7,21 @@ get '/' do
   haml :index
 end
 
-# post '/to_voice' do
-# end
+post '/upload' do
+  filename = params['cameraInput'][:filename]
+
+  File.open('uploads/' << filename, 'w') do |f|
+    f.write(params['cameraInput'][:tempfile].read)
+  end
+
+  class SomeClient
+    include HTTMultiParty
+    base_uri 'http://api.newocr.com/v1/'
+  end
+
+  response = SomeClient.post("/upload?key=#{ENV['OCR_KEY']}", :query => {
+    :my_file => File.read('uploads/' << filename)
+  }, :detect_mime_type => true)
+
+  redirect to('/')
+end
--- public/app.js
+++ public/app.js
@@ -1,50 +1,50 @@
-var form = document.getElementById("photo-form"),
-    cameraInput = document.getElementById("camera-input"),
-    submitButton = document.getElementById("submit-button");
+// var form = document.getElementById("photo-form"),
+//     cameraInput = document.getElementById("camera-input"),
+//     submitButton = document.getElementById("submit-button");
 
-form.onsubmit = function(e) {
-    event.preventDefault();
+// form.onsubmit = function(e) {
+//     event.preventDefault();
 
-    submitButton.value = "Uploading...";
+//     submitButton.value = "Uploading...";
 
-    var file = cameraInput.files[0];
+//     var file = cameraInput.files[0];
 
-    if (!file.type.match('image.*')) {
-        alert("You must upload an image file.");
-        submitButton.value = "Upload";
-        return;
-    }
+//     if (!file.type.match('image.*')) {
+//         alert("You must upload an image file.");
+//         submitButton.value = "Upload";
+//         return;
+//     }
 
-    var img = new Image();
+//     var img = new Image();
 
-    img.onload = function() {
-        var canvas = document.createElement("canvas");
-        var context = canvas.getContext("2d");
-        context.drawImage(this, 0, 0);
-        var myData = context.getImageData(0, 0, this.width, this.height);
-        var text = OCRAD(myData);
-        alert(text);
-        window.location.reload();
-    };
+//     img.onload = function() {
+//         var canvas = document.createElement("canvas");
+//         var context = canvas.getContext("2d");
+//         context.drawImage(this, 0, 0);
+//         var myData = context.getImageData(0, 0, this.width, this.height);
+//         var text = OCRAD(myData);
+//         alert(text);
+//         window.location.reload();
+//     };
 
-    img.src = URL.createObjectURL(file);
+//     img.src = URL.createObjectURL(file);
 
 
 
-    // var formData = new FormData();
+//     var formData = new FormData();
 
-    // var xhr = new XMLHttpRequest();
+//     var xhr = new XMLHttpRequest();
 
-    // xhr.open("POST", "/to_voice", true);
+//     xhr.open("POST", "/to_voice", true);
 
-    // xhr.onload = function() {
-    //     if (xhr.status === 200) {
-    //         alert("Success!");
-    //         submitButton.value = "Upload";
-    //     } else {
-    //         alert("An error occurred :/");
-    //     }
-    // };
+//     xhr.onload = function() {
+//         if (xhr.status === 200) {
+//             alert("Success!");
+//             submitButton.value = "Upload";
+//         } else {
+//             alert("An error occurred :/");
+//         }
+//     };
 
-    // xhr.send(formData);
-};
+//     xhr.send(formData);
+// };
