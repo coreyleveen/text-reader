30a8e6c48ffed7ed31d75bc6694ea1c0cff5173d
Corey Leveen <mntj11@gmail.com>
2015-06-21 22:47:26 -0400

    Add Reader class

--- app.rb
+++ app.rb
@@ -16,44 +16,18 @@ post '/upload' do
   File.open(file_path, 'w') do |f|
     f.write(params['cameraInput'][:tempfile].read)
   end
+  
 
+    text = TextReader::Reader.new(file_path).read_image
 
-  OCR_BASE_URI = "http://api.newocr.com/v1/"
-  LANG = "eng"
-  PSM = 3
-  ROTATE = 270
+    VOICE_URI = "http://api.ispeech.org/api/rest"
 
-  ocr_upload_uri = OCR_BASE_URI + "upload?key=#{ENV['OCR_KEY']}"
-
-  ocr_upload_request = RestClient::Request.new(
-                       :method => :post,
-                       :url => ocr_upload_uri,
-                       :payload => {
-                         :multipart => true,
-                         :file => File.new(file_path, 'rb')
-                       })
-
-  ocr_upload_response = JSON.parse(ocr_upload_request.execute)
-
-
-  if ocr_upload_response['status'] == 'success'
-    file_id = ocr_upload_response['data']['file_id']
-    pages = ocr_upload_response['data']['pages']
-    ocr_recognition_uri = OCR_BASE_URI + "ocr?key=#{ENV['OCR_KEY']}&file_id=#{file_id}"\
-                                         "&page=#{pages}&lang=#{LANG}&psm=#{PSM}&rotate=#{ROTATE}"
-
-    ocr_recognition_request = RestClient::Request.new(
-                              :method => :get,
-                              :url => ocr_recognition_uri
-                              )
-
-    ocr_recognition_response = JSON.parse(ocr_recognition_request.execute)
-
-    puts ocr_recognition_response['data']['text']
+    File.delete(file_path)
 
   else
     puts ocr_upload_response["status"]
     puts ocr_upload_response["message"]
+    File.delete(file_path)
   end
 
   redirect to('/')
---
+++ lib/reader.rb
@@ -0,0 +1,55 @@
+module TextReader
+  OCR_BASE_URI = "http://api.newocr/com/v1/"
+  LANG = "eng"
+  PSM = 3
+  ROTATE = 270
+
+  class Reader
+    def initialize(img_path)
+      @img_path = img_path
+      @upload_uri = OCR_BASE_URI + "upload?key=#{ENV['OCR_KEY']}"
+    end
+
+    attr_reader :image_path, :upload_uri
+
+    def read_image
+      retrieve_text(upload_image)
+    end
+
+    private
+
+    def upload_image
+      upload_request = RestClient::Request.new(
+                       :method => :post,
+                       :url => upload_uri,
+                       :payload => {
+                         :multipart => true,
+                         :file => File.new(img_path, 'rb')
+                       })
+
+      JSON.parse(upload_request.execute)
+    end
+
+    def retrieve_text(upload_response)
+      if upload_response['status'] == 'success'
+        file_id = upload_response['data']['file_id']
+        pages = upload_response['data']['pages']
+
+        recognition_uri = OCR_BASE_URI + "ocr?key=#{ENV['OCR_KEY']}&file_id=#{file_id}"\
+                          "&page=#{pages}&lang=#{LANG}&psm=#{PSM}&rotate=#{ROTATE}"
+
+        recognition_request = RestClient::Request.new(
+                              :method=> :get,
+                              :url => recognition_uri
+                              )
+
+        recognition_response = JSON.parse(recognition_request.execute)
+
+        recognition_response['data']['text']
+      else
+        raise Exception, 'Image upload failed'
+      end 
+    end
+    
+  end
+end
