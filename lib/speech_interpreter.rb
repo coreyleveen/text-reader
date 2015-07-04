8327c6177b8a14f9768a79610cbc320bb25feed4
Corey Leveen <mntj11@gmail.com>
2015-07-04 13:42:23 -0400

    Create amr files

--- lib/speech_interpreter.rb
+++ lib/speech_interpreter.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module TextReader
   class SpeechInterpreter
     def initialize(sounds)
@@ -6,6 +8,26 @@ module TextReader
     attr_reader :sounds
 
     def interpret
+      create_amr_file
+      convert_amr_to_mp3
+      send_mp3
+      delete_amr
+      delete_mp3
+    end
+
+    def create_amr_file
+      ## Create amr file with unique filename
+
+      id = SecureRandom.hex
+
+      file_name = "amr/#{id}.amr"
+
+      File.open(file_name, 'w' do |f|
+        f.write(sounds)
+      end
+    end
+
+    def delete_file
     end
   end
 end
